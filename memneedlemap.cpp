7ebd2fdfc881edde5bf27a028fbcaecc9d3b9351
huangjian <huangjian@cuav.net>
2022-02-17 14:31:34 +0800

    添加MemNeedleMap自动备份索引

--- memneedlemap.cpp
+++ memneedlemap.cpp
@@ -3,7 +3,8 @@
 
 namespace hfs {
 
-MemNeedleMap::MemNeedleMap()
+MemNeedleMap::MemNeedleMap(const std::string &indexFile)
+  : m_indexFile(indexFile.c_str())
 {
 
 }
@@ -15,44 +16,49 @@ MemNeedleMap::~MemNeedleMap()
 
 bool MemNeedleMap::Set(NeedleID id, int64_t off, uint32_t size)
 {
-    m_needleDatas.emplace_back(id, off, size);
-    m_needleIndexs.insert(std::make_pair(id, m_needleDatas.size() - 1));
-    return true;
+  m_needleDatas.emplace_back(id, off, size);
+  m_needleIndexs.insert(std::make_pair(id, m_needleDatas.size() - 1));
+  return writePutActionToFile(m_needleDatas.back());
 }
 
 NeedleValue MemNeedleMap::Get(NeedleID id)
 {
-    auto found = m_needleIndexs.find(id);
-    if (found == m_needleIndexs.end()) {
-        return NeedleValue();
-    }
+  auto found = m_needleIndexs.find(id);
+  if (found == m_needleIndexs.end()) {
+    return NeedleValue();
+  }
 
-    return m_needleDatas[found->second];
+  return m_needleDatas[found->second];
 }
 
 bool MemNeedleMap::Delete(NeedleID id)
 {
-    auto found = m_needleIndexs.find(id);
-    if (found == m_needleIndexs.end()) {
-        return false;
-    }
-    m_needleDatas.erase(m_needleDatas.begin() + static_cast<int64_t>(found->second));
-    m_needleIndexs.erase(found);
-    return true;
+  auto found = m_needleIndexs.find(id);
+  if (found == m_needleIndexs.end()) {
+    return false;
+  }
+
+  auto dataIterator = m_needleDatas.begin() + static_cast<int64_t>(found->second);
+  auto needValue = *dataIterator;
+
+  m_needleDatas.erase(dataIterator);
+  m_needleIndexs.erase(found);
+
+  return true;
 }
 
 bool MemNeedleMap::AscendingVisit(NeedleMap::VisitCallback callback)
 {
-    if (callback == nullptr) {
-        return false;
-    }
+  if (callback == nullptr) {
+    return false;
+  }
 
-    for (const auto &value : m_needleDatas) {
-        if (!callback(value)) {
-            return false;
-        }
+  for (const auto &value : m_needleDatas) {
+    if (!callback(value)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 void MemNeedleMap::Close()
@@ -62,53 +68,69 @@ void MemNeedleMap::Close()
 
 bool MemNeedleMap::WriteTo(std::ostream &os)
 {
-    if (!os) {
-        return false;
-    }
+  if (!os) {
+    return false;
+  }
 
-    return AscendingVisit([&](NeedleValue value) {
-        ByteBuffer buffer = value.ToBytes();
-        if (os.write(buffer.data(), static_cast<std::streamsize>(buffer.size()))) {
-            return true;
-        }
-        return false;
-    });
+  return AscendingVisit([&](NeedleValue value) {
+    ByteBuffer buffer = value.ToBytes();
+    if (os.write(buffer.data(), static_cast<std::streamsize>(buffer.size()))) {
+      return true;
+    }
+    return false;
+  });
 }
 
 bool MemNeedleMap::ReadFrom(std::istream &is)
 {
-    if (!is) {
-        return false;
-    }
+  if (!is) {
+    return false;
+  }
 
-    ByteBuffer buffer(NEEDLE_ID_SIZE + NEEDLE_SIZE_SIZE + NEEDLE_OFFSET_SIZE);
-    while (is.read(buffer.data(), static_cast<std::streamsize>(buffer.size()))) {
-        NeedleValue value;
-        value.ReadBytes(buffer);
-        Set(value.id(), value.offset(), value.size());
-    }
+  ByteBuffer buffer(NEEDLE_ID_SIZE + NEEDLE_SIZE_SIZE + NEEDLE_OFFSET_SIZE);
+  while (is.read(buffer.data(), static_cast<std::streamsize>(buffer.size()))) {
+    NeedleValue value;
+    value.ReadBytes(buffer);
+    Set(value.id(), value.offset(), value.size());
+  }
 
-    return true;
+  return true;
 }
 
 bool MemNeedleMap::SaveIndexToFile(const std::string &filepath)
 {
-    std::ofstream ofs(filepath.c_str(), std::ios_base::out  | std::ios_base::binary);
-    if (!ofs) {
-        return false;
-    }
+  std::ofstream ofs(filepath.c_str(), std::ios_base::out  | std::ios_base::binary);
+  if (!ofs) {
+    return false;
+  }
 
-    return WriteTo(ofs);
+  return WriteTo(ofs);
 }
 
 bool MemNeedleMap::LoadIndexFromFile(const std::string &filepath)
 {
-    std::ifstream ifs(filepath.c_str(), std::ios_base::in | std::ios_base::binary);
-    if (!ifs) {
-        return false;
-    }
+  std::ifstream ifs(filepath.c_str(), std::ios_base::in | std::ios_base::binary);
+  if (!ifs) {
+    return false;
+  }
 
-    return ReadFrom(ifs);
+  return ReadFrom(ifs);
+}
+
+bool MemNeedleMap::writePutActionToFile(const NeedleValue &value)
+{
+  ByteBuffer buffer = value.ToBytes();
+  m_indexFile.seekp(0, std::ios_base::end);
+
+  if (!m_indexFile.write(buffer.data(), buffer.size())) {
+    return false;
+  }
+  return true;
+}
+
+bool MemNeedleMap::writeDeleteActionToFile(const NeedleValue &value)
+{
+  return false;
 }
 
 }
--- memneedlemap.h
+++ memneedlemap.h
@@ -3,15 +3,16 @@
 #include "needlemap.h"
 #include <deque>
 #include <map>
+#include <fstream>
 
 namespace hfs {
 
 class MemNeedleMap : public NeedleMap
 {
-public:
-    MemNeedleMap();
+  public:
+    MemNeedleMap(const std::string &indexFile);
     ~MemNeedleMap();
-public:
+  public:
     virtual bool Set(NeedleID id, int64_t off, uint32_t size);
     virtual NeedleValue Get(NeedleID id);
     virtual bool Delete(NeedleID id);
@@ -19,12 +20,18 @@ public:
     virtual void Close();
     virtual bool WriteTo(std::ostream &os);
     virtual bool ReadFrom(std::istream &is);
-public:
+  public:
     bool SaveIndexToFile(const std::string &filepath);
     bool LoadIndexFromFile(const std::string &filepath);
-private:
+
+  protected:
+    bool writePutActionToFile(const NeedleValue &value);
+    bool writeDeleteActionToFile(const NeedleValue &value);
+
+  private:
     std::map<NeedleID, uint64_t> m_needleIndexs;
     std::deque<NeedleValue> m_needleDatas;
+    std::fstream m_indexFile;
 };
 
 }
